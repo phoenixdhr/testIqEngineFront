1ec7b390b1bc3ead71710095702b563234593490
dreyes <phoenixdhr@gmail.com>
2025-02-25 16:43:39 -0500

    Seo Robot.txt, se ha creado el archivo robots.txt.ts que genera un archivo robot.txt, se ha introducido el elemento link en seo.astro para la referencia a sitemap-index.xml, ademas se ha introducido configuracion para sidemap y configuracion para manejo de variables de entorno con archivos (.env) en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,80 @@
 
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 export default defineConfig({
   site: 'http://localhost:4321',
+
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       include: ["@apollo/client"]
     }
   },
-  integrations: [sitemap(), react()],
+  
+  env :{
+    schema:{
+      DOMINIO_DEV: envField.string({context:'server', access:'public', default:"http://localhost:4321"}),
+      DOMINIO_PREV: envField.string({context:'server', access:'public', default:"http://localhost:4322"}),
+    }
+  },
+  
+  integrations: [
+    sitemap({
+      // Frecuencia con la que cambian las páginas (opcional)
+      changefreq: 'weekly', // Valores posibles: 'always', 'hourly', 'daily', 'weekly', 'monthly', 'yearly', 'never'
+
+      // Prioridad relativa de tus páginas (0.0 a 1.0, opcional)
+      priority: 0.8,
+
+      // Fecha de última modificación global (opcional)
+      lastmod: new Date(),
+
+      // Excluir páginas específicas del sitemap
+      filter: (page) =>
+        !['/admin/', '/privado/', '/login/'].some((ruta) => page.includes(ruta)),
+
+      // Páginas externas o adicionales que quieras incluir
+      customPages: [
+        'https://tudominio.com/pagina-externa',
+        'https://tudominio.com/landing-promo',
+      ],
+
+      // Número máximo de URLs por archivo del sitemap (opcional)
+      entryLimit: 50000,
+
+      // // Función para personalizar cada entrada del sitemap (opcional)
+      // serialize(item) {
+      //   if (/blog/.test(item.url)) {
+      //     item.changefreq = 'daily';
+      //     item.priority = 0.9;
+      //     item.lastmod = new Date();
+      //   }
+      //   return item;
+      // },
+
+      // Configuración para sitios multilingües (opcional)
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es-ES',
+          en: 'en-US',
+          fr: 'fr-FR',
+        },
+      },
+
+      // Estilo visual del sitemap XML (opcional)
+      xslURL: '/sitemap.xsl',
+
+      // // Número máximo de URLs por archivo sitemap (opcional, por defecto 45000)
+      // entryLimit: 10000,
+    }),
+
+    react()],
+
+
 
 });
