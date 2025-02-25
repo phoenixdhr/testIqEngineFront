77d87c2edcd2c2a41d633b7f82548389f1a8f367
dreyes <phoenixdhr@gmail.com>
2025-02-25 11:22:11 -0500

    Se inserto metadato en el Layout, se creo el componente Seo, y el archivo site-info.ts, a parte del Layout se modifico la pagina de creacion dinamica slugCourseTitle para insertar los met<datos que ayuudan al seo

---
+++ src/const/site-info.ts
@@ -0,0 +1,9 @@
+export const siteInfo ={
+    dominio : 'http://localhost:4321',
+    titleWeb : 'Iq-Engi',
+    descripcion : 'pagina para cursos tecnicos',
+    image : 'https://res.cloudinary.com/dybzno8pq/image/upload/v1740007409/api650-w_resultado_dthber.webp',
+    type : 'website' // website, article, video.movie, music.song, book 
+
+}
+
