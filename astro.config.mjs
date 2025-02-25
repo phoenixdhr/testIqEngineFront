86a4484e1693cf9dea8f79be7d7e78d7123e9d07
dreyes <phoenixdhr@gmail.com>
2025-02-25 14:51:22 -0500

    SEO con sitemap.xml, se ha instalado la dependencia @astrojs/sitemap y se configurado astro.config, para que al momento de hacer  build, se creen los archivos automaticamente: sitemap-index.xml y sitemap-0.xml, archivos que  ayuda a los motores de búsqueda como Google a entender la estructura de tu sitio y facilita la indexación.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,18 @@
-// // @ts-check
-// import { defineConfig } from "astro/config";
-// import tailwindcss from "@tailwindcss/vite";
-// import react from "@astrojs/react";
-// // https://astro.build/config
-// export default defineConfig({
-//   vite: {
-//     plugins: [tailwindcss()],
-//   },
-
-//   integrations: [react()],
-// });
-
 
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: 'http://localhost:4321',
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       include: ["@apollo/client"]
     }
   },
-  integrations: [react()],
+  integrations: [sitemap(), react()],
+
 });
